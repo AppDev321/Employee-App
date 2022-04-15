f990b0f533f8a3d0067b0ab0d91ae5cfa49972c8
NomanHome <noman@test.com>
2022-04-15 19:56:56 +0500

    Images

---
+++ lib/pages/splash.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SplashScreenClass extends StatelessWidget {
+  const SplashScreenClass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Splash screen Demo'
+
+    );
+  }
+}
