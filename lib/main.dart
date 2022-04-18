f06d9cd006d85c680162eed631cdd41959ca4d00
noman <noman@thexsol.com>
2022-04-18 14:52:45 -0700

    Image Picker

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hnh_flutter/pages/imagepicker.dart';
 import 'package:hnh_flutter/pages/login.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
 
 void main() {
-  runApp(MyApp());
+  //runApp(CameraWidget());
+  runApp(
+      MaterialApp(
+        title: "Pick Image Camera",
+        home: CameraWidget() ,
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
