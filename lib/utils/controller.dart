36b376d24773eff290484213c7eb76b180870dd0
bilalehsan <bilal.ehsan@yahoo.com>
2022-10-11 12:51:37 +0500

    attendance changes

--- lib/utils/controller.dart
+++ lib/utils/controller.dart
@@ -11,8 +11,6 @@ import '../pages/login/login.dart';
 class Controller {
   static const String appBaseURL = "http://vmi808920.contaboserver.net/api";
  // "http://192.168.1.21:8000/api";
-
-
   final String auth_token = "auth_token";
   final String loginRemember = "login_remember";
   static const double leftCardColorMargin = 5;
