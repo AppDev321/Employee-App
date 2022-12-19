579153167403ac39e4c90d9e4b17d01ca3365a32
bilalehsan <bilal.ehsan@yahoo.com>
2022-12-19 15:28:59 +0500

    Attachment Queries

--- lib/pages/chat/component/audio_chat_bubble.dart
+++ lib/pages/chat/component/audio_chat_bubble.dart
@@ -29,104 +29,93 @@ class _AudioBubbleState extends State<AudioBubble> {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4),
+    return SingleChildScrollView(
       child: Row(
-     mainAxisAlignment: MainAxisAlignment.end,
+       mainAxisAlignment: MainAxisAlignment.end,
         children: [
-          //SizedBox(width: MediaQuery.of(context).size.width * 0.4),
+
           Expanded(
-            child: Container(
-              height: 45,
-              padding: const EdgeInsets.only(left: 12, right: 18),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-               // color:primaryColor,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // const SizedBox(height: 4),
-                  Row(
-                    children: [
-                      StreamBuilder<PlayerState>(
-                        stream: player.playerStateStream,
-                        builder: (context, snapshot) {
-                          final playerState = snapshot.data;
-                          final processingState = playerState?.processingState;
-                          final playing = playerState?.playing;
-                          if (processingState == ProcessingState.loading ||
-                              processingState == ProcessingState.buffering) {
-                            return GestureDetector(
-                              child: const Icon(Icons.play_arrow, ),
-                              onTap: player.play,
-                            );
-                          } else if (playing != true) {
-                            return GestureDetector(
-                              child: const Icon(Icons.play_arrow),
-                              onTap: player.play,
-                            );
-                          } else if (processingState !=
-                              ProcessingState.completed) {
-                            return GestureDetector(
-                              child: const Icon(Icons.pause),
-                              onTap: player.pause,
-                            );
-                          } else {
-                            return GestureDetector(
-                              child: const Icon(Icons.replay),
-                              onTap: () {
-                                player.seek(Duration.zero);
-                              },
-                            );
-                          }
+            child:
+            Row(
+              children: [
+                StreamBuilder<PlayerState>(
+                  stream: player.playerStateStream,
+                  builder: (context, snapshot) {
+                    final playerState = snapshot.data;
+                    final processingState = playerState?.processingState;
+                    final playing = playerState?.playing;
+                    if (processingState == ProcessingState.loading ||
+                        processingState == ProcessingState.buffering) {
+                      return GestureDetector(
+                        child: const Icon(Icons.play_arrow, ),
+                        onTap: player.play,
+                      );
+                    } else if (playing != true) {
+                      return GestureDetector(
+                        onTap: player.play,
+                        child:  const Icon(Icons.play_arrow),
+                      );
+                    } else if (processingState !=
+                        ProcessingState.completed) {
+                      return GestureDetector(
+
+                        onTap: player.pause,
+                        child: const Icon(Icons.pause),
+                      );
+                    } else {
+                      return GestureDetector(
+                        onTap: () {
+                          player.seek(Duration.zero);
                         },
-                      ),
-                      const SizedBox(width: 8),
-                      Expanded(
-                        child: StreamBuilder<Duration>(
-                          stream: player.positionStream,
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              return Column(
-                                children: [
-                                  const SizedBox(height: 4),
-                                  LinearProgressIndicator(
-                                    value: snapshot.data!.inMilliseconds /
-                                        (duration?.inMilliseconds ?? 1),
+                        child: const Icon(Icons.replay),
+                      );
+                    }
+                  },
+                ),
+                const SizedBox(width: 8),
+                Expanded(
+                  child: StreamBuilder<Duration>(
+                    stream: player.positionStream,
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Column(
+                          children: [
+                            const SizedBox(height: 4),
+                            LinearProgressIndicator(
+                              minHeight: 2,
+                              backgroundColor:Colors.grey,
+                              value: snapshot.data!.inMilliseconds /
+                                  (duration?.inMilliseconds ?? 1),
+                            ),
+
+                            const SizedBox(height: 6),
+                            Row(
+                              mainAxisAlignment:
+                              MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  prettyDuration(
+                                      snapshot.data! == Duration.zero
+                                          ? duration ?? Duration.zero
+                                          : snapshot.data!),
+                                  style: const TextStyle(
+                                    fontSize: 10,
+
                                   ),
+                                ),
+                                 CustomTextWidget(text: prettyDuration(duration!),size: 10)
 
-                                  const SizedBox(height: 6),
-                                  Row(
-                                    mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                    children: [
-                                      Text(
-                                        prettyDuration(
-                                            snapshot.data! == Duration.zero
-                                                ? duration ?? Duration.zero
-                                                : snapshot.data!),
-                                        style: const TextStyle(
-                                          fontSize: 10,
-
-                                        ),
-                                      ),
-                                      const CustomTextWidget(text: "02:36",size: 10)
-
-                                    ],
-                                  ),
-                                ],
-                              );
-                            } else {
-                              return const LinearProgressIndicator();
-                            }
-                          },
-                        ),
-                      ),
-                    ],
+                              ],
+                            ),
+                          ],
+                        );
+                      } else {
+                        return const LinearProgressIndicator();
+                      }
+                    },
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
@@ -137,6 +126,6 @@ class _AudioBubbleState extends State<AudioBubble> {
   String prettyDuration(Duration d) {
     var min = d.inMinutes < 10 ? "0${d.inMinutes}" : d.inMinutes.toString();
     var sec = d.inSeconds < 10 ? "0${d.inSeconds}" : d.inSeconds.toString();
-    return min + ":" + sec;
+    return "$min:$sec";
   }
 }
