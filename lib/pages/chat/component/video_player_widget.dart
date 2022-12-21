730f3fcc96beabb0636852b9a7306836772595df
Ghulam Farid <ghulamfarid96.gf@gmail.com>
2022-12-21 14:23:06 +0500

    video player widget

---
+++ lib/pages/chat/component/video_player_widget.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerScreen extends StatefulWidget {
+  final String path;
+
+  const VideoPlayerScreen({Key? key, required this.path}) : super(key: key);
+
+  @override
+  State<VideoPlayerScreen> createState() => _VideoPlayerScreenState();
+}
+
+class _VideoPlayerScreenState extends State<VideoPlayerScreen> {
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
+
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(widget.path);
+
+    // Initialize the controller and store the Future for later use.
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    // Use the controller to loop the video.
+    _controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: [
+              FutureBuilder(
+                future: _initializeVideoPlayerFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    // If the VideoPlayerController has finished initialization, use
+                    // the data it provides to limit the aspect ratio of the video.
+                    return AspectRatio(
+                      aspectRatio: _controller.value.aspectRatio,
+                      // Use the VideoPlayer widget to display the video.
+                      child: VideoPlayer(_controller),
+                    );
+                  } else {
+                    // If the VideoPlayerController is still initializing, show a
+                    // loading spinner.
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  color: Colors.black38,
+                  width: MediaQuery.of(context).size.width,
+                  padding:const EdgeInsets.symmetric(vertical: 5, horizontal: 8),
+                  child: TextFormField(
+                    style:const TextStyle(
+                      color: Colors.white,
+                      fontSize: 17,
+                    ),
+                    maxLines: 6,
+                    minLines: 1,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Add Caption....",
+                        prefixIcon:const Icon(
+                          Icons.add_photo_alternate,
+                          color: Colors.white,
+                          size: 27,
+                        ),
+                        hintStyle:const TextStyle(
+                          color: Colors.white,
+                          fontSize: 17,
+                        ),
+                        suffixIcon: InkWell(
+                          onTap: (){},
+                          child:const CircleAvatar(
+                            radius: 27,
+                            child: Icon(
+                              Icons.check,
+                              color: Colors.white,
+                              size: 27,
+                            ),
+                          ),
+                        )),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      );
+
+
+
+      Scaffold(
+      appBar: AppBar(
+        title: const Text('Butterfly Video'),
+      ),
+      // Use a FutureBuilder to display a loading spinner while waiting for the
+      // VideoPlayerController to finish initializing.
+      body: FutureBuilder(
+        future: _initializeVideoPlayerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the VideoPlayerController has finished initialization, use
+            // the data it provides to limit the aspect ratio of the video.
+            return AspectRatio(
+              aspectRatio: _controller.value.aspectRatio,
+              // Use the VideoPlayer widget to display the video.
+              child: VideoPlayer(_controller),
+            );
+          } else {
+            // If the VideoPlayerController is still initializing, show a
+            // loading spinner.
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
